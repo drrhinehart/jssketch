8024c8ff3ad0a49bee1936417f8364acc1d97625
Dallas Rhinehart <dallasrhinehart@gmail.com>
2017-05-14 11:22:04 -1000

    Was unable to figure out a way to make the size of the pixels adjustable

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 $(function() {
-    for (var x = 0; x < 50; x++) {
-        for (var y = 0; y < 50; y++) {
+    for (var x = 0; x < 100; x++) {
+        for (var y = 0; y < 100; y++) {
             $("<div>").addClass("box").appendTo('#screen');
         }
     }
 });
 
-/*function btn(){
-	var number;
-	var grid = prompt("Please enter a number between 1-64");
-};*/
-
-
 $(document).ready(function() {
     $(".box").hover(function() {
-        $(this).css("background-color","#13B2AA"); //edit, body must be in quotes!
+     $(this).css("background-color","#13B2AA"); 
     });
 });
 
  function btn() {
-     location.reload();
+    location.reload();
 };
+
+//couldn't figure out how to make the size of the pixels adjustable
